690a70408919d462c93eeb5a3d43ecdf8b3a9be2
cheery <cheerybill@gmail.com>
2024-09-16 21:01:17 +0800

    update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useMemo } from "react"
+import {createTheme, ThemeProvider} from "@mui/material/styles"
+import { themeSettings } from "./theme"
+import { CssBaseline } from "@mui/material"
 
 function App() {
 
+  const theme = useMemo(()=> createTheme(themeSettings),[])
+
   return (
     <>
-      <div>
+      <div className="app">
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+        </ThemeProvider>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
