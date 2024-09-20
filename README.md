15dadc68e57b1050346c745bf943891fd6746a3b
cheery <cheerybill@gmail.com>
2024-09-20 14:47:34 +0800

    last upate

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ Tech Stacks : React 、react-redux、react-router、recharts、typescript
 <br/>
 技术栈 ：....
 
+responsive design
+
 i like this beautiful landing page for this consist of a lot of kpis and information that business persons can use to display their information about key performance indicators and other types of data that they need to keep track of their finances for their company
 <br/>
 展示KPI，追踪数据
 <br/>
+
+Learning Sources ：ed-roh -> https://github.com/ed-roh/finance-app
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { CssBaseline, Box } from "@mui/material";
-
+import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
-import { themeSettings } from "@/theme";
 import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
 function App() {
-
-  const theme = useMemo(() => createTheme(themeSettings), [])
-
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <>
-      <div className="app">
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/predictions" element={<div>predictions page</div>}/>
-              </Routes>
-            </Box>
-          </ThemeProvider>
-        </BrowserRouter>
-      </div>
-    </>
-  )
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
